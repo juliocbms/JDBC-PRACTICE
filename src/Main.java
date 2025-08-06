e29cea8e7338c352dc5947f8196154444eb34624
juliocbms <juliocesar13b@gmail.com>
2025-08-05 21:56:30 -0300

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import db.DB;
 import db.DbException;
+import db.DbIntegrityException;
 
 import java.sql.*;
 import java.text.ParseException;
@@ -32,12 +33,14 @@ public class Main {
 //            DB.closeConnection();
 //        }
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+/*
 
+        //INSERIR DADOS
         Connection conn = null;
         PreparedStatement st = null;
         try {
             conn = DB.getConnection();
-/*
+
             st = conn.prepareStatement(
                     "INSERT INTO seller "
                     + "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
@@ -50,7 +53,7 @@ public class Main {
             st.setDate(3,new  java.sql.Date(sdf.parse("22/04/1985").getTime()));
             st.setDouble(4,3000.0);
             st.setInt(5,4);
-*/
+
             st = conn.prepareStatement(
                     "insert into department (Name) values ('D1'),('D2')",
                     Statement.RETURN_GENERATED_KEYS);
@@ -74,5 +77,100 @@ public class Main {
             DB.closeStatement(st);
             DB.closeConnection();
         }
+        */
+
+        //ATUALIZAR DADOS
+        /*Connection conn = null;
+        PreparedStatement st = null;
+
+        try {
+            conn = DB.getConnection();
+            st = conn.prepareStatement(
+                    "UPDATE seller "
+                    + "SET BaseSalary = BaseSalary + ? "
+                    + "WHERE "
+                    + "(DepartmentId = ?)");
+            st.setDouble(1,200.0);
+            st.setInt(2,2);
+
+            int rowsAffected = st.executeUpdate();
+
+            System.out.println("Done! Rows Affected: "+ rowsAffected);
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        finally {
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
+*/
+
+        /*
+    //DELETAR DADOS
+     Connection conn = null;
+        PreparedStatement st = null;
+
+        try {
+            conn = DB.getConnection();
+            st = conn.prepareStatement(
+                    "DELETE FROM department "
+                    + "WHERE "
+                    + "Id = ?");
+
+            st.setInt(1,2);
+
+            int rowsAffected = st.executeUpdate();
+
+            System.out.println("Done! Rows Affected: "+ rowsAffected);
+        }catch (SQLException e){
+            throw new DbIntegrityException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
+*/
+
+/*
+
+        //TRANSAÇÕES
+        Connection conn = null;
+       Statement st = null;
+
+        try {
+            conn = DB.getConnection();
+
+            conn.setAutoCommit(false);
+
+            st = conn.createStatement();
+
+            int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1");
+
+//            int x =1;
+//            if (x<2){
+//                throw new SQLException("Fake error");
+//            }
+
+            int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2");
+
+            conn.commit();
+
+            System.out.println("rows1 = "+rows1);
+            System.out.println("rows2 = "+rows2);
+
+        }catch (SQLException e){
+            try {
+                conn.rollback();
+                throw  new DbException("transaction rolled back! Caused by: "+ e.getMessage());
+            } catch (SQLException ex) {
+                throw new DbException("Error trying to rollback! Caused by: "+ex.getMessage());
+            }
+        }
+        finally {
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
+
+ */
     }
 }
